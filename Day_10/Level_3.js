253421b3ce72bef4495f9986dc63a6deed3e2183
Praneeth Priyankara <ndppriyankara@gmail.com>
2024-03-19 07:38:29 +0530

    "adding repos"

--- Day_10/Level_3.js
+++ Day_10/Level_3.js
@@ -3,12 +3,20 @@ const countries = require('./countries')
 // 1) How many languages are there in the countries object file.
 let languages = [];
 countries.forEach((obj) => languages.push(obj.languages))
-languagesFlattened = languages.flat()
+let languagesFlattened = languages.flat()
 let Languages = new Set(languagesFlattened)
 
 // 2) Use the countries data to find the 10 most spoken languages
 function mostSpokenLanguages(arr, count) {
-
+  let languages = [];
+  arr.forEach((obj) => languages.push(obj.languages))
+  let languagesFlattened = languages.flat()
+  let Languages = new Set(languagesFlattened)
+  let langCount = [];
+  Languages.forEach((cou) => langCount.push((languagesFlattened.filter((item) => item === cou)).length))
+  let returnObj = [];
+  let langArray = Languages.values()
+  console.log(langArray)
 }
 
-mostSpokenLanguages()
+mostSpokenLanguages(countries, 15)
