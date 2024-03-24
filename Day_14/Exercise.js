10cadd926e5fb9b46d9f3a5bc8e6de95ae9c1b85
Praneeth Priyankara <ndppriyankara@gmail.com>
2024-03-24 08:55:20 +0530

    "Day 14 Exercises"

---
+++ Day_14/Exercise.js
@@ -0,0 +1,12 @@
+let firstName = 'Praneeth'
+try {
+  let fullName = firstName + lastName
+  console.log(fullName)
+}
+catch (err) {
+  console.log(err)
+}
+finally {
+  console.log('This is the full name')
+}
+
