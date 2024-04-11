3ecb6ecdee33adfb66aaa77b4df275a654e7f7f1
Praneeth Priyankara <ndppriyankara@gmail.com>
2024-04-11 08:13:25 +0530

    finshing Day 22 Level 02

--- Day_22/Level_2/index.js
+++ Day_22/Level_2/index.js
@@ -1,9 +1,14 @@
 const countriesAPI = 'https://restcountries.com/v2/all'
 
 let worldCountries = document.querySelector('h5')
+let title = document.querySelector('h2')
+let author = document.querySelector('p')
 let cardDiv = document.createElement('div')
 document.body.appendChild(cardDiv)
 
+worldCountries.style.textAlign = 'center'
+title.style.textAlign = 'center'
+author.style.textAlign = 'center'
 cardDiv.style.display = 'grid';
 cardDiv.style.gridTemplateColumns = 'repeat(5, 1fr)';
 cardDiv.style.gap = '10px'
@@ -23,6 +28,8 @@ let fetchingCountries = async () => {
 function fetched(item) {
   let arr = [];
   item.forEach((e) => arr.push(e.name))
+  let flags = [];
+  item.forEach((e) => flags.push(e.flags.png))
   let string = `Total Number of Countries : ${arr.length}`
   worldCountries.textContent = string;
   for (let i = 0; i < arr.length; i++) {
@@ -30,15 +37,30 @@ function fetched(item) {
     innerCards.className = 'inner-cards'
     innerCards.id = `inner-card${i + 1}`
     innerCards.style.width = '220px'
-    innerCards.style.height = '350px'
+    innerCards.style.height = '200px'
     innerCards.style.border = '1px solid black'
     let innerName = document.createElement('div')
     innerName.className = 'inner-name'
     innerCards.appendChild(innerName)
     innerName.style.width = 'inherit'
-    innerName.style.height = '70px'
+    innerName.style.height = '50px'
     innerName.textContent = `${arr[i]}`
+    innerName.style.textAlign = 'center'
+    innerName.style.borderBottom = '1px solid black'
+    innerName.style.marginTop = '18px'
+    innerName.style.marginBottom = '6px'
+    innerName.style.fontFamily = 'lucidaConsole'
+    let innerFlag = document.createElement('div')
+    let image = document.createElement('img')
+    image.src = `${flags[i]}`
+    image.style.width = '160px'
+    image.style.height = '100px'
+    image.style.margin = '4px 0 4px 0'
+    innerFlag.style.display = 'flex'
+    innerFlag.style.justifyContent = 'center'
     cardDiv.appendChild(innerCards)
+    innerCards.appendChild(innerFlag)
+    innerFlag.appendChild(image)
   }
 }
 fetchingCountries().then((elem) => {fetched(elem)}).catch((error) => console.log(error))
