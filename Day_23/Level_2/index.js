65b1d775174c0d94d9c798576ebb7eb012bb13bf
Praneeth Priyankara <ndppriyankara@gmail.com>
2024-04-23 09:11:14 +0530

    finishing Day 23

--- Day_23/Level_2/index.js
+++ Day_23/Level_2/index.js
@@ -1,12 +1,29 @@
-let pressedKey = document.querySelector('#pressed-key')
-let pressedKeyCode = document.querySelector('#key-code')
-let paragraph = document.querySelector('p')
-let keyCodeDiv = document.querySelector('#div-key-code')
+let paragraph = document.querySelector("p");
+let keyCodeDiv = document.querySelector("#div-key-code");
+keyCodeDiv.style.display = 'none'
 
-keyCodeDiv.innerHTML = '';
-paragraph.textContent = 'Press any keyboard key'
+let keyCode = document.createElement('span')
+keyCode.classList.add('key-code')
+keyCode.style.color =  "#1aa646";
+keyCode.style.textShadow =  "2px 2px #dedcdb";
+keyCode.style.margin =  "10pt";
+keyCode.style.marginTop =  "20pt";
+keyCode.style.transform =  "translateY(30%)";
+keyCode.style.padding =  "0";
+keyCode.style.fontSize =  "70pt";
+keyCode.style.fontFamily =  "Montserrat, Courier, monospace";
 
-document.addEventListener('keydown', function(e) {
-  pressedKey.textContent = e.key
-  pressedKeyCode.textContent = e.code.charCodeAt()
-})
+document.addEventListener("keydown", function (e) {
+  paragraph.textContent = "You Pressed ";
+  let spanElement = document.createElement("span");
+  spanElement.classList.add("pressed-key");
+  spanElement.textContent = e.key;
+  spanElement.style.color = "#1aa646";
+  paragraph.appendChild(spanElement);
+  
+  keyCode.textContent = e.key.charCodeAt();
+  keyCodeDiv.style.display = 'block'
+  keyCodeDiv.style.marginTop = '30pt'
+  keyCodeDiv.style.boxShadow = '5px 5px 4px 4px #dedcdb'
+  keyCodeDiv.appendChild(keyCode)
+});
---
+++ Day_23/README.md
@@ -0,0 +1,6 @@
+## Day 23
+
+#### Exercise : Level 1
+
+
+#### Exercise : Level 2
