2fd792afb7dc5bd0493478505a8c01531acd8a0c
Praneeth Priyankara <ndppriyankara@gmail.com>
2024-05-04 08:59:47 +0530

    finishing Day 25 Level 1

--- Day_25/Level_1/index.js
+++ Day_25/Level_1/index.js
@@ -13,6 +13,17 @@ const fetchingData = async () => {
     });
     let array = [];
     array.push(sortedPopulationArray);
+
+    let languagesArray = [];
+    let languagesNameArray = [];
+    response.forEach((elem) => {
+      languagesArray.push(elem.languages)
+    })
+    languagesArray.forEach((item) => {
+      item.forEach(t => languagesNameArray.push(t.name))
+    })
+    array.push(languagesNameArray)
+
     return array;
   } catch (error) {
     console.warn(error);
@@ -22,9 +33,12 @@ const fetchingData = async () => {
 let populationButton = document.querySelector(".population");
 let languageButton = document.querySelector(".languages");
 
+let dataBoxText = document.querySelector('.data-box-text')
+
 let dataChartBox = document.querySelector(".data-charts");
 
 populationButton.addEventListener("click", populationDisplay);
+languageButton.addEventListener("click", languagesDisplay);
 
 async function populationDisplay() {
   let arrayNew = await fetchingData();
@@ -37,17 +51,22 @@ async function populationDisplay() {
   }, 0);
   newPopulationArray.push({ name: "World", population: countedPopulation });
   populationDataSet.forEach((a) => newPopulationArray.push(a));
+
+dataBoxText.textContent = '10 Most populated countries in the world'
+
+  dataChartBox.innerHTML = ""
+
   for (let i = 0; i < 10; i++) {
     let divNew = document.createElement("div");
     divNew.setAttribute("class", `div-box-${i}`);
-    let countryName = document.createElement("p");
-    countryName.textContent = `${newPopulationArray[i].name}`;
-    countryName.style.margin = "8px 35px 8px 35px";
-    countryName.style.fontFamily = "Montserrat, monospace";
-    countryName.style.fontSize = "14pt";
-    countryName.style.textAlign = "right";
-    countryName.style.width = "90px";
-    divNew.appendChild(countryName);
+    let countryNameDiv = document.createElement("div");
+    countryNameDiv.textContent = `${newPopulationArray[i].name}`;
+    countryNameDiv.style.margin = "8px 35px 8px 35px";
+    countryNameDiv.style.fontFamily = "Montserrat, monospace";
+    countryNameDiv.style.fontSize = "14pt";
+    countryNameDiv.style.textAlign = "right";
+    countryNameDiv.style.width = "120pt";
+    divNew.appendChild(countryNameDiv);
 
     let mainVal = newPopulationArray[0].population;
     let percentage = Math.floor(
@@ -58,6 +77,7 @@ async function populationDisplay() {
     boxDiv.setAttribute("class", `box-div-${i}`);
     boxDiv.style.width = "500pt";
     boxDiv.style.height = "50pt";
+    boxDiv.style.marginLeft = "20pt";
 
     let chartDiv = document.createElement("div");
     chartDiv.setAttribute("class", `chart-div-${i}`);
@@ -71,16 +91,82 @@ async function populationDisplay() {
     divNew.appendChild(boxDiv);
 
     let countryPopulation = document.createElement("div");
-    countryPopulation.textContent = `${newPopulationArray[i].population}`;
+    countryPopulation.textContent = `${newPopulationArray[i].population.toLocaleString()}`;
     countryPopulation.style.margin = "8px 35px 8px 35px";
     countryPopulation.style.fontFamily = "Montserrat, monospace";
     countryPopulation.style.fontSize = "14pt";
+    countryPopulation.style.width = "120pt";
+    countryPopulation.style.textAlign = "left";
     divNew.appendChild(countryPopulation);
 
     divNew.style.display = "flex";
     divNew.style.justifyContent = "center";
     divNew.style.alignItems = "center";
-    divNew.style.marginBottom = "30pt";
+    divNew.style.marginBottom = "10pt";
+
+    dataChartBox.appendChild(divNew);
+  }
+}
+
+async function languagesDisplay() {
+  let fetchedData = await fetchingData()
+  let languagesDataSet = fetchedData[1]
+  let LanguageDataSet = new Set(languagesDataSet)
+  let languageCountedArray = [];
+  for (let i of LanguageDataSet) {
+    let filtered = languagesDataSet.filter((e) => e === i)
+    languageCountedArray.push({name: i, count: filtered.length})
+  }
+let languageCountedArraySorted = languageCountedArray.sort((a, b) => {
+    return b.count - a.count
+  })
+
+dataBoxText.textContent = '10 Most Spoken languages in the world'
+
+  dataChartBox.innerHTML = ""
+
+  for (let j = 0; j < 10; j++) {
+    let divNew = document.createElement('div')
+    divNew.setAttribute('class', `lang-div-${j}`)
+
+    let langParagraph = document.createElement('div')
+    langParagraph.textContent = languageCountedArraySorted[j].name
+    langParagraph.style.margin = "8px 35px 8px 35px";
+    langParagraph.style.fontFamily = "Montserrat, monospace";
+    langParagraph.style.fontSize = "14pt";
+    langParagraph.style.textAlign = "right";
+    langParagraph.style.width = "90pt";
+    divNew.appendChild(langParagraph)
+
+    let langBox = document.createElement('div')
+    langBox.setAttribute('class', `lang-box-div-${j}`)
+    langBox.style.height = '50pt'
+    langBox.style.width = '500pt'
+
+    let langChartDiv = document.createElement("div");
+    langChartDiv.setAttribute("class", `chart-div-${j}`);
+    langChartDiv.style.border = "none";
+    langChartDiv.style.height = "inherit";
+    langChartDiv.style.border = "none";
+    langChartDiv.style.backgroundColor = "#fb8004";
+    langChartDiv.style.width = `${languageCountedArraySorted[j].count * 5}pt`;
+
+    langBox.appendChild(langChartDiv)
+    divNew.appendChild(langBox)
+
+    let languageNumber = document.createElement("div");
+    languageNumber.textContent = `${languageCountedArraySorted[j].count}`;
+    languageNumber.style.margin = "8px 35px 8px 35px";
+    languageNumber.style.fontFamily = "Montserrat, monospace";
+    languageNumber.style.fontSize = "14pt";
+    languageNumber.style.width = "20pt";
+    languageNumber.style.textAlign = "left";
+    divNew.appendChild(languageNumber);
+
+    divNew.style.display = "flex";
+    divNew.style.justifyContent = "center";
+    divNew.style.alignItems = "center";
+    divNew.style.marginBottom = "10pt";
 
     dataChartBox.appendChild(divNew);
   }
