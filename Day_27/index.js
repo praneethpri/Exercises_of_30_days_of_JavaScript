6fe49d642f174d2524d3f22e17099b38ca527858
Praneeth Priyankara <ndppriyankara@gmail.com>
2025-03-07 09:22:11 +0530

    adding index.js into Day_27

---
+++ Day_27/index.js
@@ -0,0 +1,47 @@
+let introductoryTag = document.querySelector(".introductory-tag");
+let introductoryEmoji = document.querySelector(".introductory-emoji");
+let technologiesWords = document.querySelector(".technologies");
+
+let tagLines = [
+  "Quick Learner",
+  "Aspiring Developer",
+  "Technology Advocate",
+  "Linux Enthusiast",
+];
+let tagLineEmojis = [
+  "&#128218",
+  "&#128104;&#8205;&#128187;",
+  "&#127793",
+  "&#128039",
+];
+let technologyColors = [
+  { tech: "HTML", color: "#E34F26" }, // HTML - Strong Red-Orange
+  { tech: "CSS", color: "#1572B6" }, // CSS - Classic Blue
+  { tech: "JavaScript", color: "#F7DF1E" }, // JavaScript - Bright Yellow
+  { tech: "React", color: "#61DAFB" }, // React - Light Blue
+  { tech: "Angular", color: "#DD0031" }, // Angular - Red
+  { tech: "NextJS", color: "#000000" }, // NextJS - Black
+  { tech: "NestJS", color: "#E0234E" }, // NestJS - Dark Pink
+  { tech: "MongoDB", color: "#47A248" }, // MongoDB - Green
+  { tech: "MySQL", color: "#00618A" }, // MySQL - Blue
+  { tech: "Express", color: "#000000" }, // Express - Black
+  { tech: "NodeJS", color: "#8CC84B" }, // NodeJS - Green
+  { tech: "Python", color: "#306998" }, // Python - Blue
+  { tech: "Django", color: "#092E20" }, // Django - Dark Green
+  { tech: "Git", color: "#F05032" }, // Git - Red
+  { tech: "Linux", color: "#FF6A00" }, // Linux - Orange
+];
+
+var i = 0;
+function technologiesOnRepeat() {
+  setTimeout(() => {
+    technologiesWords.innerHTML = `${technologyColors[i].tech}`;
+    technologiesWords.style.color = `${technologyColors[i].color}`;
+    i++;
+    if (i < technologyColors.length) {
+      technologiesOnRepeat();
+    }
+  }, 2000);
+}
+
+technologiesOnRepeat();
